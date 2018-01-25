a3d35edd128817deb547bfdc5bc93df8a6b69fff
Steven Inouye <steveninouye@msn.com>
2018-01-24 20:45:27 -1000

    hfdfs

--- index.js
+++ index.js
@@ -8,35 +8,7 @@ function sort(array){
         return null;
     };
     let swaps = 0;
-    for (let i = 0; i < array.length-1; i++){
-        if(array[i] === null){
-        } else if (array[i+1] === null){
-            swaps++;
-            array[i+1] = array[i];
-            array[i] = null;
-        } else if (array[i] === Infinity && (typeof array[i+1]) === 'number'){
-            swaps++;
-            array[i] = array[i+1];
-            array[i+1] = Infinity;
-        } else if (array[i+1] === Infinity && typeof array[i] === 'string'){
-            swaps++;
-            array[i+1] = array[i];
-            array[i] = Infinity;
-        } else if(array[i+1] === Infinity || array[i] === Infinity){
-        } else if (typeof array[i+1] === 'number' & typeof array[i] === 'number'){
-            if (array[i] > array[i+1]){
-                swaps++;
-                let cache = array[i];
-                array[i] = array[i+1];
-                array[i+1] = cache;
-            }
-        } else if (array[i].toString() > array[i+1].toString()){
-            swaps++;
-            let cache = array[i];
-            array[i] = array[i+1];
-            array[i+1] = cache;
-        }
-    }
+    
     if (swaps === 0){
         return array;
     } else {
